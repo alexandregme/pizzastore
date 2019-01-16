8fbfe6820488bb8892b5da549c32ec453cee4282
Alexandre Mendes <alexandregme@gmail.com>
2019-01-16 20:24:37 -0200

    [redux][cart-item] implementing function to remove pizza from cart

--- components/__tests__/cart-item.js
+++ components/__tests__/cart-item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import expect from 'expect';
-import CartItem from '../cart-item';
+import { CartItem } from '../cart-item';
 import Button from "../button";
 
 describe("Cart Item", ()=>{
--- components/cart-item.js
+++ components/cart-item.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { removeFromCart } from "../actions";
 import Button from "./button";
 
-const CartItem = ({cartItem = {pizzaSize: 'cart item'}}) =>(
+export const CartItem = ({cartItem = {pizzaSize: 'cart item'}, handleRemoveFromCart}) =>(
   <React.Fragment>
     <p>{cartItem.pizzaSize}</p>
     <Button
       label={`remove item ${cartItem.id}`}
-      handleClick={() => {}}
+      handleClick={() => {handleRemoveFromCart(cartItem.id);}}
     />
   </React.Fragment>
 );
 
 CartItem.propTypes = {
-  cartItem: PropTypes.object
+  cartItem: PropTypes.object,
+  handleRemoveFromCart: PropTypes.func
 };
 
-export default CartItem;
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  handleRemoveFromCart: () => dispatch(removeFromCart(ownProps.cartItem.id))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CartItem);
