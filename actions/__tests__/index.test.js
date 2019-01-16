5874fcc790c4b4981aa9ac683a0380ae87e0c971
Alexandre Mendes <alexandregme@gmail.com>
2019-01-16 00:16:39 -0200

    [actions][addToCart] intial config

---
+++ actions/__tests__/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import expect from 'expect';
+import {addToCart, removeFromCart} from '../../actions';
+
+describe('Actions', () => {
+  it('should create an action to pizzas to add cart', () => {
+    const pizzaSize = 'SMALL';
+    const expectedAction = {
+      type: 'ADD_TO_CART',
+      pizzaSize
+    };
+    expect(addToCart(pizzaSize)).toEqual(expectedAction);
+  });
+});
---
+++ actions/index.js
@@ -0,0 +1,4 @@
+export const addToCart = pizzaSize => ({
+  type: 'ADD_TO_CART',
+  pizzaSize
+});
