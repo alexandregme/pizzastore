19e04691bb0ce36cdf52d272563f0b1db73ec850
Alexandre Mendes <alexandregme@gmail.com>
2019-01-17 16:57:25 -0200

    [component][cart-item] fixing test

--- components/__tests__/cart-item.js
+++ components/__tests__/cart-item.js
@@ -17,10 +17,22 @@ describe("Cart Item", ()=>{
     expect(mountedCartItem).toMatchSnapshot();
   });
 
-  it('expect to have default value for cart item', () => {
-    let cartItem = mountedCartItem.find('p');
-    expect(cartItem).toHaveLength(1);
-    expect(cartItem.text()).toEqual('cart item');
+  it('expect to have default value for item name', () => {
+    let itemNameValue = mountedCartItem.find('p.pizzaName');
+    expect(itemNameValue).toHaveLength(1);
+    expect(itemNameValue.text()).toEqual('default pizza');
+  });
+
+  it('expect to have default value for item max toppings', () => {
+    let itemMaxToppingValue = mountedCartItem.find('p.pizzaMaxToppings');
+    expect(itemMaxToppingValue).toHaveLength(1);
+    expect(itemMaxToppingValue.text()).toEqual("0");
+  });
+
+  it('expect to have default value for item base price', () => {
+    let itemBasePriceValue = mountedCartItem.find('p.pizzaBasePrice');
+    expect(itemBasePriceValue).toHaveLength(1);
+    expect(itemBasePriceValue.text()).toEqual("0");
   });
 
   it('expect to have Button component', () => {
@@ -36,21 +48,21 @@ describe("Cart Custom component props", ()=>{
     mountedCartItem = shallow(<CartItem {...props} />);
   });
 
-  it('expect to have default value for pizza name', () => {
-    let pizzaNameValue = mountedPizza.find('p.pizzaName');
-    expect(pizzaNameValue).toHaveLength(1);
-    expect(pizzaNameValue.text()).toEqual('custom pizza');
+  it('expect to have default value for item name', () => {
+    let itemNameValue = mountedCartItem.find('p.pizzaName');
+    expect(itemNameValue).toHaveLength(1);
+    expect(itemNameValue.text()).toEqual('custom pizza');
   });
 
-  it('expect to have default value for pizza max toppings', () => {
-    let pizzaMaxToppingValue = mountedPizza.find('p.pizzaMaxToppings');
-    expect(pizzaMaxToppingValue).toHaveLength(1);
-    expect(pizzaMaxToppingValue.text()).toEqual("1");
+  it('expect to have default value for item max toppings', () => {
+    let itemMaxToppingValue = mountedCartItem.find('p.pizzaMaxToppings');
+    expect(itemMaxToppingValue).toHaveLength(1);
+    expect(itemMaxToppingValue.text()).toEqual("1");
   });
 
-  it('expect to have default value for base price', () => {
-    let pizzaBasePriceValue = mountedPizza.find('p.pizzaBasePrice');
-    expect(pizzaBasePriceValue).toHaveLength(1);
-    expect(pizzaBasePriceValue.text()).toEqual("1");
+  it('expect to have default value for item base price', () => {
+    let itemBasePriceValue = mountedCartItem.find('p.pizzaBasePrice');
+    expect(itemBasePriceValue).toHaveLength(1);
+    expect(itemBasePriceValue.text()).toEqual("1");
   });
 });
