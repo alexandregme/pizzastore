60704ef71ecfd8a415b564dea62fc9768c7ea79c
Alexandre Mendes <alexandregme@gmail.com>
2019-01-18 12:04:22 -0200

    [pages] refactoring to remove component from react as it does not need lyficle for now

--- pages/cart.js
+++ pages/cart.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import App from '../components/app';
 import Cart from '../components/cart';
 
-class CartPage extends Component {
-    render() {
-        return (
-            <App title="Cart Page">
-                <Cart />
-            </App>
-        );
-    }
-}
+export const CartPage = () =>(
+    <App title="Cart Page">
+        <Cart />
+    </App>
+);
 
 export default CartPage;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import App from "../components/app";
 import Pizzas from "../components/pizzas";
 import Cart from "../components/cart";
 
-class IndexPage extends Component {
-    render() {
-        return (
-            <App title="Home Page">
-              <React.Fragment>
-                <Pizzas />
-                <Cart />
-              </React.Fragment>
-            </App>
-        );
-    }
-}
+export const IndexPage = () =>(
+    <App title="Home Page">
+      <React.Fragment>
+        <Pizzas />
+        <Cart />
+      </React.Fragment>
+    </App>
+);
 
 export default IndexPage;
